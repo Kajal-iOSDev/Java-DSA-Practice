88b153a7b370a018a796e8492c3724df389a7b24
Kajal-iOSDev <mekajal.kumari7061@gmail.com>
2026-04-22 01:51:39 +0530

    Create Day1.java

---
+++ Day1.java
@@ -0,0 +1,11 @@
+public class Day1 {
+    public static void main(String[] args) {
+        // Day 1: Starting Java Journey
+        System.out.println("Java Placement Prep: Started!");
+         
+         // Basic sum logic
+        int a = 10;
+        int b = 20;
+        System.out.println("Sum is: " + (a + b));
+    }
+}
